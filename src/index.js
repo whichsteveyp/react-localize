851f4009245560d0847d4ab49298d9aa7160074a
Stephen Rivas Jr <sprjr@me.com>
2018-06-15 14:19:55 -0700

    remove default export because rollup or Rich Harris warned me too

--- src/index.js
+++ src/index.js
@@ -2,4 +2,3 @@ import { LocalizationProvider, LocalizationConsumer } from './react-localize';
 import withLocalization from './withLocalization';
 
 export { LocalizationProvider, LocalizationConsumer, withLocalization };
-export default LocalizationConsumer;
