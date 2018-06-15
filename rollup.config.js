b359de9c90f14ecabff073d01e759dfd76383396
Stephen Rivas Jr <sprjr@me.com>
2018-06-15 14:11:45 -0700

    be more like @jamesplease

---
+++ rollup.config.js
@@ -0,0 +1,50 @@
+import nodeResolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+import babel from 'rollup-plugin-babel';
+import uglify from 'rollup-plugin-uglify';
+import replace from 'rollup-plugin-replace';
+
+var env = process.env.NODE_ENV;
+var config = {
+  format: 'umd',
+  moduleName: 'ReactLocalize',
+  external: ['react', 'prop-types'],
+  globals: {
+    react: 'React',
+    'prop-types': 'PropTypes',
+  },
+  context: 'this',
+  plugins: [
+    nodeResolve({
+      jsnext: true
+    }),
+    commonjs({
+      include: 'node_modules/**'
+
+      // explicitly specify unresolvable named exports
+      // (see below for more details)
+      // namedExports: { './module.js': ['foo', 'bar' ] },  // Default: undefined
+    }),
+    babel({
+      exclude: 'node_modules/**'
+    }),
+    replace({
+      'process.env.NODE_ENV': JSON.stringify(env)
+    })
+  ]
+};
+
+if (env === 'production') {
+  config.plugins.push(
+    uglify({
+      compress: {
+        pure_getters: true,
+        unsafe: true,
+        unsafe_comps: true,
+        warnings: false
+      }
+    })
+  );
+}
+
+export default config;
---
+++ src/util.format.js
@@ -0,0 +1,42 @@
+// I used to use `util.format()` which was massive, then I switched to
+// format-util, although when using rollup I discovered that the index.js
+// just exported `require('util').format`, and then had the below contents
+// in another file. at any rate all I want is this function:
+
+function format(fmt) {
+  var re = /(%?)(%([jds]))/g
+    , args = Array.prototype.slice.call(arguments, 1);
+  if(args.length) {
+    fmt = fmt.replace(re, function(match, escaped, ptn, flag) {
+      var arg = args.shift();
+      switch(flag) {
+        case 's':
+          arg = '' + arg;
+          break;
+        case 'd':
+          arg = Number(arg);
+          break;
+        case 'j':
+          arg = JSON.stringify(arg);
+          break;
+      }
+      if(!escaped) {
+        return arg;
+      }
+      args.unshift(arg);
+      return match;
+    })
+  }
+
+  // arguments remain after formatting
+  if(args.length) {
+    fmt += ' ' + args.join(' ');
+  }
+
+  // update escaped %% values
+  fmt = fmt.replace(/%{2,2}/g, '%');
+
+  return '' + fmt;
+}
+
+export default format;
