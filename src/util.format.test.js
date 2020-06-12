92c4756388b79315582043acd80a5fdf36f01289
Julian Hartig <julian@whisper-net.de>
2020-06-13 01:07:03 +0200

    Add tests for format with multiple placeholders, substitutes given as array. Fix test case for single JSON placeholder.

--- src/util.format.test.js
+++ src/util.format.test.js
@@ -13,8 +13,19 @@ test('formats a string properly when values are provided', () => {
   const numberFormatted = format('%d items in cart', [5]);
   expect(numberFormatted).toBe('5 items in cart');
 
-  const jsonFormatted = format('Source Codez: %j', [{ source: 'code' }]);
+  const jsonFormatted = format('Source Codez: %j', [ [{ source: 'code' }] ]);
   expect(jsonFormatted).toBe('Source Codez: [{\"source\":\"code\"}]');
+});
+
+test('also works with multiple values', () => {
+  const formatted = format('Hello, %s %s', ['Stephen', 'Myers']);
+  expect(formatted).toBe('Hello, Stephen Myers');
+
+  const numberFormatted = format('%d of %d items in cart', [5, 10]);
+  expect(numberFormatted).toBe('5 of 10 items in cart');
+
+  const jsonFormatted = format('Source Codez: %j %j', [ [{ source: 'code' }], { id: 'object identifier' } ]);
+  expect(jsonFormatted).toBe('Source Codez: [{\"source\":\"code\"}] {\"id\":\"object identifier\"}');
 });
 
 test('does not explode when a message is missing', () => {
